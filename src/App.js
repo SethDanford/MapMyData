a008f5824fe63c922ecf64889aa453d88234b6d7
Seth Danford <s3845408@student.rmit.edu.au>
2022-03-27 23:04:55 +1100

    Added Static page
    Added a cool static page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text">
+          192.168.1.4.61983@anonymous ~ %
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
